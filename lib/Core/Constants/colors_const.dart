4072cd76acd6814f16652edeb8cad63481c3206c
Ahmaad22 <ahmaad.ali.abdulrahman@gmail.com>
2023-10-10 21:48:18 +0300

    initalize repo

---
+++ lib/Core/Constants/colors_const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ConstColors {
+  static const backgroundColor = Color(0xFFFFFFFF);
+  static const primaryColor = Color(0xFF6A4515);
+  static const blackColor = Color(0xff040404);
+  static const greyColor = Color(0xFF757575);
+}
---
+++ lib/Core/Routes/routes.dart
@@ -0,0 +1,21 @@
+
+
+
+// import 'package:flutter/material.dart';
+
+// class Routes {
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch (settings.name) {
+//       case SplashView.routeName:
+//         return MaterialPageRoute(
+//           builder: (context) => const SplashView(),
+//           settings: settings,
+//         );
+//       default:
+//         return MaterialPageRoute(
+//           builder: (context) => const SplashView(),
+//           settings: settings,
+//         );
+//     }
+//   }
+// }
